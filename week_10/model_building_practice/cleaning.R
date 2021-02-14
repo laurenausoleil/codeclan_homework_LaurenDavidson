a9b94838194dbff57a97ef173b778e4039bb8876
laurenausoleil <lauren.au.soleil@gmail.com>
2021-02-14 17:54:47 +0000

    change year to character and fix is_weekday column

--- week_10/model_building_practice/cleaning.R
+++ week_10/model_building_practice/cleaning.R
@@ -16,8 +16,9 @@ avocado_price <- read_csv("data/avocado.csv") %>%
 avocado_price <- avocado_price %>% 
   # add date columns for possible analysis 
   mutate(
-    is_weekday = if_else(wday(ymd(date)) == 1:5, "yes", "no"),
-    month = month(ymd(date), label = TRUE)
+    is_weekday = if_else(wday(ymd(date)) %in% 1:5, "yes", "no"),
+    month = month(ymd(date), label = TRUE),
+    year = as.character(year)
   ) %>% 
 # drop unhelpful columns
 select(-c(x1, total_bags, date)) 
